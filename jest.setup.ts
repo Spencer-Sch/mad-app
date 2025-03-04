597b5639aaae39401b103d61e5d6b6ace661ef5c
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2025-03-04 06:55:56 -0600

    configure jest

---
+++ jest.setup.ts
@@ -0,0 +1,3 @@
+// solution to '.toBeInTheDocument()' not being available on `expect()`
+// https://github.com/testing-library/jest-dom?tab=readme-ov-file#with-jestglobals
+import '@testing-library/jest-dom/jest-globals'
