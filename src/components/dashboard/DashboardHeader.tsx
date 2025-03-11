7041ebbc5bae94c85e8f9c7773958263ba367548
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2025-03-11 15:04:23 -0500

    DashboardHeader: add mobile only AppSidebar trigger

--- src/components/dashboard/DashboardHeader.tsx
+++ src/components/dashboard/DashboardHeader.tsx
@@ -1,7 +1,12 @@
+import { SidebarTrigger } from '../ui/sidebar'
+import { sidebarNames } from '@/lib/constants'
+
 export default function DashboardHeader() {
+	const { appSidebar } = sidebarNames
 	return (
-		<header className={`w-full h-[--layout-header-height]`}>
+		<header className="w-full h-[--layout-header-height]">
 			<h1>Dashboard Header</h1>
+			<SidebarTrigger name={appSidebar} className="flex md:hidden" />
 		</header>
 	)
 }
