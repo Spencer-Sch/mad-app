e6010e9d2d3afff7dbde025584f402372cb4fe99
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2025-03-11 15:05:35 -0500

    CourseHeader: add mobile only CourseSidebar trigger and 'home' nav btn

--- src/components/course/CourseHeader.tsx
+++ src/components/course/CourseHeader.tsx
@@ -1,7 +1,24 @@
+import { RxHome } from 'react-icons/rx'
+import Link from 'next/link'
+import { SidebarTrigger } from '../ui/sidebar'
+import { sidebarNames } from '@/lib/constants'
+import { buttonVariants } from '@/components/ui/button'
+
 export default function CourseHeader() {
+	const { courseSidebar } = sidebarNames
 	return (
-		<header className={`w-full h-[--layout-header-height]`}>
+		<header className="w-full h-[--layout-header-height]">
 			<h1>Course Header</h1>
+			<div className="flex justify-between md:hidden">
+				<SidebarTrigger name={courseSidebar} />
+				<Link
+					href="/dashboard"
+					className={buttonVariants({ variant: 'ghost', size: 'icon' })}
+				>
+					<RxHome />
+					<span className="sr-only">Go Home</span>
+				</Link>
+			</div>
 		</header>
 	)
 }
