021db3b471699a05f68638ceb7fabd448f87a7dd
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2025-03-14 13:30:45 -0500

    add useDeviceDetect hook

---
+++ src/hooks/useDeviceDetect.tsx
@@ -0,0 +1,58 @@
+import { useCallback, useEffect, useState } from 'react'
+import { useDebounce } from './useDebouce'
+
+export function useDeviceDetect() {
+	const [isTouchDevice, setIsTouchDevice] = useState(false)
+
+	const checkTouchCapability = useCallback(() => {
+		const touchDetect = 'ontouchstart' in window || navigator.maxTouchPoints > 0
+		setIsTouchDevice(touchDetect)
+	}, [])
+
+	const debouncedCheckCapability = useDebounce(checkTouchCapability, 500)
+
+	useEffect(() => {
+		checkTouchCapability()
+
+		const handlePointerDown = (e: PointerEvent) => {
+			if ((e.pointerType === 'touch') !== isTouchDevice) {
+				checkTouchCapability()
+			}
+		}
+
+		const handleResize = () => {
+			console.log('resize')
+			debouncedCheckCapability()
+		}
+
+		const handleVisibilityChange = () => {
+			if (document.visibilityState === 'visible') {
+				checkTouchCapability()
+			}
+		}
+
+		const handleOrientationChange = () => {
+			checkTouchCapability()
+		}
+
+		window.addEventListener('pointerdown', handlePointerDown, { passive: true })
+		// window.addEventListener('resize', debouncedCheckCapability, {
+		// 	passive: true,
+		// })
+		window.addEventListener('resize', handleResize, {
+			passive: true,
+		})
+		window.addEventListener('orientationchange', handleOrientationChange)
+		document.addEventListener('visibilitychange', handleVisibilityChange)
+
+		return () => {
+			window.removeEventListener('pointerdown', handlePointerDown)
+			// window.removeEventListener('resize', debouncedCheckCapability)
+			window.removeEventListener('resize', handleResize)
+			window.removeEventListener('orientationchange', handleOrientationChange)
+			document.removeEventListener('visibilitychange', handleVisibilityChange)
+		}
+	}, [isTouchDevice])
+
+	return isTouchDevice
+}
