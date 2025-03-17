d07d6d6dc65957df4edf6d799f5642b22a5f6cb2
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2025-03-17 11:37:46 -0500

    add reusable DashbaordCard component

---
+++ src/components/dashboard/card/DashboardCard.tsx
@@ -0,0 +1,41 @@
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from '@/components/ui/card'
+import { ReactNode } from 'react'
+
+interface Props {
+	title: string
+	description?: string
+	content: ReactNode
+	footer?: ReactNode
+	isLoading?: boolean
+}
+
+export default function DashboardCard({
+	title,
+	content,
+	description,
+	footer,
+	isLoading,
+}: Props) {
+	if (isLoading) {
+		return <Card className="w-full">{/* Loading skeleton here */}</Card>
+	}
+
+	return (
+		<Card className="w-full">
+			<CardHeader>
+				<CardTitle className="text-2xl">
+					{title ? title : 'Card Title'}
+				</CardTitle>
+				{description && <CardDescription>{description}</CardDescription>}
+			</CardHeader>
+			{content ? content : <CardContent>Content, content, content</CardContent>}
+			{footer && footer}
+		</Card>
+	)
+}
